7df8a130295a1eefcbefb68b7829985bf929fd18
Jenny-F <42142271+Jenny-F@users.noreply.github.com>
2018-09-07 23:27:00 +0400

    Добавлен код, проверяющий наличие пустых полей

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,17 @@
 var btn = document.querySelector(".search-hotel-btn");
 var popup = document.querySelector(".modal-hotels");
+var form = popup.querySelector(".search-hotel-form");
+var arrivalDate = popup.querySelector("[name = arrival-date]");
+var departureDate = popup.querySelector("[name = departure-date]");
 
 btn.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
-})
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrivalDate.value || !dapertireDate.value) {
+    evt.preventDefault();
+    console.log("Нужно ввести даты заезда и выезда");
+  }
+});
