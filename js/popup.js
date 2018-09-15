dbe0df406558ce57b1748aeea7e198be290fa73d
Jenny-F <42142271+Jenny-F@users.noreply.github.com>
2018-09-15 18:50:53 +0400

    Добавлено удаление класса у модалки

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,8 @@ var form = popup.querySelector(".search-hotel-form");
 var arrivalDate = popup.querySelector("[name = arrival-date]");
 var departureDate = popup.querySelector("[name = departure-date]");
 
+popup.classList.remove("modal-show");
+
 btn.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.toggle("modal-show");
